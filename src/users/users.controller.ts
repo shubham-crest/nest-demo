7abada1cf9dcf02366a9a22b8d186db39f0e58ee
shubham-crest <shubham.t@crestinfosystems.com>
2024-11-21 12:16:27 +0530

    added user controller and service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // '/users' route
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get() // GET /users or /user?role=value&age=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.userService.findAll(role);
+  }
+
+  @Get('interns') // GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    // Once @Param('id') is used everything after /users/xyz route will be considered as :id (id = xyz), so any other /users/pqr route must be declared above.
+    return this.userService.findOne(+id);
+  }
+
+  @Post() // POST /users | nest automatically send 201 status code
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.userService.create(user);
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.userService.update(+id, userUpdate);
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return this.userService.delete(+id);
+  }
+}
